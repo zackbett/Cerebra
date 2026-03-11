b91041bcd2fc6a482748d56a7b784548f015b747
MAJYY <101084335+zackbett@users.noreply.github.com>
2026-03-11 15:13:08 +0700

    send captured knowledge to cerebra engine

--- src/agent/neuronAgent.ts
+++ src/agent/neuronAgent.ts
@@ -14,6 +14,7 @@ export class NeuronAgent {
     }
 
     const knowledge = captureKnowledge(discovery)
+    this.engine.storeMemory(knowledge)
 
     console.log("Agent captured knowledge:", knowledge)
 
